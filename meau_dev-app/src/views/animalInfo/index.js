1c4626bc0275601e2f2cf27aa2aec3ae9ed3fd03
Silva <octavio.silva@vetta.digital>
2023-06-06 01:47:37 -0300

    Add animal details screen

--- meau_dev-app/src/views/animalInfo/index.js
+++ meau_dev-app/src/views/animalInfo/index.js
@@ -1,15 +1,112 @@
-import React, { useState } from "react";
-import { ScrollView, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ScrollView, Image, View, Text, TouchableOpacity, SafeAreaView } from "react-native";
 import styles from "./styles.style";
+import { getByName, remove } from "../../../services/animal";
 
 const AnimalInfo = ({ route, navigation }) => {
   const [loading, setLoading] = useState(true);
-  console.log(route.params);
   const animal = route.params;
+  const [animalId, setAnimalId] = useState('');
+  const [age, setAge] = useState('');
+  const [size, setSize] = useState('');
+  const [gender, setGender] = useState('');
+  const [temperance, setTemperance] = useState([]);
+  const [guard, setGuard] = useState([]);
+  const [health, setHealth] = useState([]);
+  const [vaccinated, setVaccinated] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const animalsData = await getByName(animal.name)
+        console.log(animalsData)
+        setAnimalDetails(animalsData[0])
+        setAnimalId(animalsData[1]);
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+    fetchData();
+  }, []);
+
+  function setAnimalDetails(animal) {
+    setAge(animal.age);
+    setSize(animal.size);
+    setGender(animal.gender);
+    setTemperance(animal.temperance);
+    setHealth(animal.health);
+  }
+  const handleInterested = () => {
+    // todo
+  };
+
+  const handleRemovePet = () => {
+    // remove(animalId);
+    // navigation.navigate('Meus Pets')
+  };
 
   return (
     <ScrollView style={styles.container}>
-      <Text>{animal.name}</Text>
+      <Image style={styles.image} source={{ uri: animal.imageBase64 }} />
+      <Text style={styles.namePet}>{animal.name}</Text>
+      <SafeAreaView style={styles.alingItems}>
+        <View>
+          <Text style={styles.label}>Sexo</Text>
+          <Text style={styles.content}>{gender}</Text>
+        </View>
+        <View>
+          <Text style={styles.label}>Porte</Text>
+          <Text style={styles.content}>{size}</Text>
+        </View>
+        <View>
+          <Text style={styles.label}>Idade</Text>
+          <Text style={styles.content}>{age}</Text>
+        </View>
+      </SafeAreaView>
+      <Text style={styles.label}>Localização</Text>
+      <Text style={styles.content}>TODO</Text>
+      <SafeAreaView style={styles.alingItems}>
+        <View >
+          <Text style={styles.label} >Castrado</Text>
+          <Text style={styles.content}></Text>
+        </View>
+        <View>
+          <Text style={styles.label}>Vermificado</Text>
+          <Text style={styles.content}></Text>
+        </View>
+      </SafeAreaView>
+      <SafeAreaView style={styles.alingItems}>
+        <View>
+          <Text style={styles.label}>Vacinado</Text>
+          <Text style={styles.content}>Sim</Text>
+        </View>
+        <View>
+          <Text style={styles.label}>Doenças</Text>
+          <Text style={styles.content}>Doenças</Text>
+        </View>
+      </SafeAreaView>
+      <View >
+        <Text style={styles.label}>Temperamento</Text>
+        <Text style={styles.content}></Text>
+
+        <Text style={styles.label}>O {animal.name} precisa de</Text>
+        <Text style={styles.content}>O {animal.name} precisa de</Text>
+
+        <Text style={styles.label}>Exigências do doador</Text>
+        <Text style={styles.content}>Exigências do doador</Text>
+
+        <Text style={styles.label}>Mais sobre {animal.name}</Text>
+        <Text style={styles.content}>Mais sobre {animal.name}</Text>
+
+      </View>
+      <View style={styles.alingItems}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Ver interessados</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText} onPress={() => handleRemovePet()}>Remover Pet</Text>
+        </TouchableOpacity>
+      </View>
     </ScrollView>
   );
 };
--- meau_dev-app/src/views/animalInfo/styles.style.js
+++ meau_dev-app/src/views/animalInfo/styles.style.js
@@ -1,4 +1,5 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
+const {width, height} = Dimensions.get('window')
 
 const styles = StyleSheet.create({
   loadingContainer: {
@@ -11,9 +12,46 @@ const styles = StyleSheet.create({
     width: "100%",
     backgroundColor: "#fafafa",
   },
-  pets: {
-    flex: 1
+  image: {
+    width: "100%",
+    aspectRatio: width / (height/3)
   },
+  namePet: {
+    color: '#434343',
+    fontSize: 14,
+    marginBottom: 10,
+    marginTop: 10,
+    fontWeight: 'bold',
+  },
+  button: {
+    width: '40%',
+    height: 40,
+    backgroundColor: '#88c9bf',
+    borderRadius: 3,
+    marginBottom: 10,
+    marginTop: 15,
+  },
+  buttonText: {
+    fontSize: 18,
+    textAlign: 'center',
+    justifyContent: 'center',
+    color: '#434343',
+  },
+  alingItems: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  label: {
+    fontSize: 14,
+    color: '#589b9b',
+    fontFamily: "Roboto_400Regular",
+  },
+  content: {
+    marginBottom: 10,
+    color: '#757575',
+    fontSize: 16,
+  }
 });
 
 export default styles;
